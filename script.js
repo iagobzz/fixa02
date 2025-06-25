850b95e3b06060842aeb8aa998ed007cbcd9ebf4
iagobzz <iagobarboza@uniplaclages.edu.br>
2025-06-24 21:40:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function carregar() {
+    const msg = document.getElementById('msg')
+    const img = document.getElementById('imagem')
+    const data = new Date()
+    const hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = '#515154'
+    }
+}
